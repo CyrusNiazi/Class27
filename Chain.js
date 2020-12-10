f622914a3db9efad19a330561c4c134bbe4a69e4
CyrusNiazi <72956148+CyrusNiazi@users.noreply.github.com>
2020-12-09 17:40:45 -0800

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,22 @@
+class Chain{
+
+    constructor(bodyP,bodyQ){
+        var options={
+            bodyA: bodyP,
+            bodyB: bodyQ,
+            stiffness:0.4,
+            length:10
+            }      
+                this.chain= Constraint.create(options);
+                World.add(world,this.chain);
+
+    }
+
+
+    display(){
+        var pointA= this.chain.bodyA.position;
+        var pointB= this.chain.bodyB.position;
+        strokeWeight(4)
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
